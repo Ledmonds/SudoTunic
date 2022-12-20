5b4cab514a5d6753028bc1f157bcd7ed46b51eab
Leuan Edmonds <leuan.edmonds@gmail.com>
2022-12-20 19:30:13 +0100

    chore: Adding in ability to push buttons

---
+++ public/javascripts/controller.js
@@ -0,0 +1,45 @@
+const buttonPushed = {
+  listeners: [],
+  addListener(cb) {
+    this.listeners.push(cb);
+  },
+  signal(state) {
+    for (const cb of this.listeners) {
+      cb(state);
+    }
+  }
+}
+
+const event = new Event('buttonPushed');
+  
+var buttonstate = false;
+var pushedButtons = [];
+
+const loop = () => {
+  const gamepads = navigator.getGamepads();
+  const gamepad = gamepads[0];
+
+  if (gamepad) {
+    const statenow = gamepad.buttons.some(btn => btn.pressed);
+    if (buttonstate !== statenow) {
+
+      gamepad.buttons.map(e => e.pressed).forEach((isPressed, buttonIndex) => {
+        if(isPressed) {
+          pushedButtons.push(buttonIndex);
+          console.log(pushedButtons);
+        }
+      })
+
+      buttonstate = statenow;
+      buttonPushed.signal(statenow);
+    }
+  }
+
+  setTimeout(loop, 10);
+}
+
+buttonPushed.addListener((buttonState) => {
+  console.log(`${buttonState} Pushed`);
+});
+
+loop();
