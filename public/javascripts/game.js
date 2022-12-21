0913e55727a830c1f6b521eeeba74b50ca95b1d3
Leuan Edmonds <leuan.edmonds@gmail.com>
2022-12-21 21:06:02 +0100

    fix: Resolving an issue where the `chime` ingame sound would trigger 3 times for each game step

--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -80,8 +80,6 @@ function startGame()
 function stepGame()
 { 
     chime.play();
-    chime.play();
-    chime.play();
     buttonsPushed = [];
     $("#"+levels[currentLevel].Id).toggleClass("transparent");
     currentLevel++;
